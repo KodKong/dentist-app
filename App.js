cf61c80f00737db51267189c5e910bcd9e20c4db
unknown <muzafarovroma@list.ru>
2021-02-22 23:30:01 +0500

    работа с кнопками

--- App.js
+++ App.js
@@ -1,31 +1,106 @@
 import React from 'react';
-import { Group, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Group, StyleSheet, Text, View, TouchableOpacity, SectionList } from 'react-native';
 import styled from "styled-components/native";
 import Groups from "./Components/Groups";
+import { Ionicons } from '@expo/vector-icons';
+
+const DATA = [
+  {
+    title: "15 марта", 
+    data: [
+      {
+      time: `15:30`,  
+      procedure: `чистка`,
+      active: true, 
+      user: {
+        fullname: "Роман Музафаров", 
+        avatar: 'https://sun9-18.userapi.com/impg/Ae_Dfzl-W7xAD644yEulDPwmD4NHL6vGDhxSrg/HD0APVDEcsU.jpg?size=1333x2000&quality=96&proxy=1&sign=da5538fac8c1ca9cb802da6e071dbbc2&type=album'
+      }
+      }, 
+      {
+        time: `15:30`,  
+        procedure: `чистка`,
+        active: true, 
+        user: {
+          fullname: "Алина Высоцкая", 
+          avatar: 'https://i.pinimg.com/564x/c4/d5/21/c4d5213de9546ddc4285529455bce991.jpg'
+        }
+      },
+      
+    ]
+  }, 
+  {
+    title: "16 марта", 
+    data: [
+      {
+      time: `15:30`,  
+      procedure: `чистка`,
+      active: true, 
+      user: {
+        fullname: "Роман Музафаров", 
+        avatar: 'https://sun9-18.userapi.com/impg/Ae_Dfzl-W7xAD644yEulDPwmD4NHL6vGDhxSrg/HD0APVDEcsU.jpg?size=1333x2000&quality=96&proxy=1&sign=da5538fac8c1ca9cb802da6e071dbbc2&type=album'
+      }
+      }, 
+      {
+        time: `15:30`,  
+        procedure: `чистка`,
+        active: true, 
+        user: {
+          fullname: "Алина Высоцкая", 
+          avatar: 'https://i.pinimg.com/564x/c4/d5/21/c4d5213de9546ddc4285529455bce991.jpg'
+        }
+      }
+    ]
+  }
+]
+
+
 
 export default function App() {
   return (
    <Container>
-    <Groups title={"15 марта"}
-    items={[
-      {
-      time: `15:30`,  
-      user: {
-        fullname: "Роман Музафаров", 
-        procedure: `чистка`,
-      }
-      }
-    ]}
-    
-    ></Groups>
+
+    <SectionList 
+     sections={DATA}
+      keyExtractor={(item, index) =>  index}
+      renderItem={({item}) => <Groups {...item}/>}
+      renderSectionHeader={({section: {title} }) => (
+        <GroupTittle>{title}</GroupTittle>
+      )}/>
+     <PlusButton>
+     <Ionicons name="ios-add" size={26} color="#fff" />
+     </PlusButton>
     </Container>  
   );
 }
+
+const PlusButton = styled.TouchableOpacity`
+
+align-items: center; 
+justify-content: center; 
+border-radius: 50px;
+width: 64px; 
+height: 64px; 
+background: #2A86FF; 
+position: absolute; 
+bottom: 15px; 
+right: 28px; 
+
+`;
 
 
 const Container = styled.View`
 
   flex: 1; 
   margin-top: 50px; 
+  padding: 0 20px; 
+  margin-bottom: 25px; 
 
+`; 
+
+const GroupTittle = styled.Text`
+font-weight: 800; 
+font-size: 22px; 
+color: #000000; 
+margin-top: 20px
 `; 
--- Components/Groups.jsx
+++ Components/Groups.jsx
@@ -3,21 +3,20 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import styled from "styled-components/native";
 
 
-const Groups = ({items, title}) => {
+const Groups = ({user, procedure, active, time}) => {
     return (
-        <Wrapper>
-        <GroupTittle>{title}</GroupTittle>
-        {items.map((item, index) =>  <GroupItem key={index}>
+       
+         <GroupItem>
           <Avatar source = {{
-            uri: 'https://sun9-18.userapi.com/impg/Ae_Dfzl-W7xAD644yEulDPwmD4NHL6vGDhxSrg/HD0APVDEcsU.jpg?size=1333x2000&quality=96&proxy=1&sign=da5538fac8c1ca9cb802da6e071dbbc2&type=album'
+            uri: user.avatar
           }}/>
           <View style={{ flex: 1 }}>
-          <FullName>{item.user.fullname}</FullName>
-          <GrayText>{item.user.procedure}</GrayText>
+          <FullName>{user.fullname}</FullName>
+          <GrayText>{procedure}</GrayText>
           </View>
-          <GroupeDate active>{item.time}</GroupeDate>
-        </GroupItem>)} 
-      </Wrapper>
+          <GroupeDate active={active}>{time}</GroupeDate>
+        </GroupItem>
+    
     )
 }
 
@@ -26,66 +25,56 @@ Groups.defaultProps = {
 }
 
 
-const GroupTittle = styled.Text`
-font-weight: 800; 
-font-size: 22px; 
-color: #000000; 
-`; 
+  const GroupItem = styled.TouchableOpacity`
 
-const GroupItem = styled.TouchableOpacity`
+  align-items: center; 
+  padding: 20px 0; 
+  flex-direction: row; 
+  border-bottom-width: 1px; 
+  border-bottom-color: #f3f3f3; 
 
-align-items: center; 
-padding: 20px 0; 
-flex-direction: row; 
-border-bottom-width: 1px; 
-border-bottom-color: #f3f3f3; 
+  `;
 
-`;
+  const GroupeDate = styled.Text`
 
-const GroupeDate = styled.Text`
+  background: ${props => props.active ? '#2A86FF' : '#E9F5FF' }; 
+  border-radius: 18px; 
+  font-weight: 600; 
+  color: ${props => props.active ? '#fff' : '#4294ff' }; 
+  font-size: 14px; 
+  width: 70px; 
+  height: 32px; 
+  text-align: center; 
+  line-height: 28px; 
 
-background: ${props => props.active ? '#2A86FF' : '#E9F5FF' }; 
-border-radius: 18px; 
-font-weight: 600; 
-color: ${props => props.active ? '#fff' : '#4294ff' }; 
-font-size: 14px; 
-width: 70px; 
-height: 32px; 
-text-align: center; 
-line-height: 28px; 
-
-`; 
+  `; 
 
 
-const FullName = styled.Text`
+  const FullName = styled.Text`
 
-font-weight: 600; 
-font-size: 16px; 
+  font-weight: 600; 
+  font-size: 16px; 
 
-`; 
-
-
-const GrayText = styled.Text`
-
-font-size: 16px; 
-color: #8b979f; 
-
-`; 
-
-const Wrapper = styled.View`
-
-padding: 0 20px
-
-`;
-
-const Avatar = styled.Image`
-
-margin-right: 15px
-border-radius: 50px; 
-width: 40px; 
-height: 40px
-
-`; 
+  `; 
 
 
-export default Groups
+  const GrayText = styled.Text`
+
+  font-size: 16px; 
+  color: #8b979f; 
+
+  `; 
+
+  
+
+  const Avatar = styled.Image`
+
+  margin-right: 15px
+  border-radius: 50px; 
+  width: 40px; 
+  height: 40px
+
+  `; 
+
+
+  export default Groups
